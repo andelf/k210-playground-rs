c97369095d66cf530fb793adfd5eb13c67afda09
Andelf <andelf@gmail.com>
2020-11-22 13:53:03 +0800

    examples: refine blinky

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,3 +1,6 @@
+//! Blinky of onboard RGB LED.
+//!
+//! Board: Maixduino
 #![no_std]
 #![no_main]
 
@@ -7,7 +10,7 @@ use k210_hal::stdout::Stdout;
 use k210_hal::{fpioa, gpio::Gpio, pac, prelude::*};
 use panic_halt as _;
 
-use maixduino_playground::delay::McycleDelay;
+use k210_playground::delay::McycleDelay;
 
 #[riscv_rt::entry]
 fn main() -> ! {
@@ -25,21 +28,33 @@ fn main() -> ! {
     // delay
     let mut delay = McycleDelay::new(&clocks);
 
-    // led blue
+    // LEDs
     let gpio = p.GPIO.split(&mut sysctl.apb0);
+
+    let io13 = fpioa.io13.into_function(fpioa::GPIO5);
+    let mut red = Gpio::new(gpio.gpio5, io13).into_push_pull_output();
+
+    let io12 = fpioa.io12.into_function(fpioa::GPIO4);
+    let mut green = Gpio::new(gpio.gpio4, io12).into_push_pull_output();
+
     let io14 = fpioa.io14.into_function(fpioa::GPIO6);
     let mut blue = Gpio::new(gpio.gpio6, io14).into_push_pull_output();
+
+    // disable LCD backlight
+    let io17 = fpioa.io17.into_function(fpioa::GPIO7);
+    let mut back_light = Gpio::new(gpio.gpio7, io17).into_push_pull_output();
+    back_light.set_high().unwrap();
 
     blue.set_low().ok();
 
     writeln!(stdout, "Waking on LED...").unwrap();
 
     loop {
-        writeln!(
-            stdout,
-            "toggle..."
-        )
-        .unwrap();
+        writeln!(stdout, "toggle...").unwrap();
+        delay.delay_ms(500);
+        red.toggle().ok();
+        delay.delay_ms(500);
+        green.toggle().ok();
         delay.delay_ms(500);
         blue.toggle().ok();
     }
